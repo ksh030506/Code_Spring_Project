3f0b9fafc367566e10e72ca9b9864ec746c13c60
ksh030506 <ksh030506@naver.com>
2020-03-31 20:17:47 +0900

    Part 2 add
    스프링 MVC 설정 익숙해 지기
    - 생성과 동작 과정
    - 구조 이해와 예제 등
    - 파일 업로드 처리
    - 예외 처리 (1개는 못함)

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,15 +1,27 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,6 +30,13 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+	
+	//바인딩 => 파라미터 변환
+	@InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-mm-dd");
+        binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+    } 
 	
 	@RequestMapping("")
 	public void basic() {
@@ -71,5 +90,75 @@ public class SampleController {
 		System.out.println("list dtos : " + list);
 		return "ex02Bean";
 	}
+	
+	
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		System.out.println("todo : " + todo);
+		return "ex03";
+	}
+	
+	
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		
+		System.out.println("dto : " + dto);
+		System.out.println("page : " + page);
+		log.info("dto : " + dto);
+		log.info("page : " + page);
+		
+		return "/sample/ex04";
+	}
+	
+	
+	@GetMapping("/ex05")
+	public void ex05() {
+		System.out.println("ex05.......");
+	}
+	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		System.out.println("ex06...........");
+		
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(11);
+		dto.setName("김상현");
+		
+		return dto;
+	}
+	
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07(){
+		System.out.println("ex07.........");
+		
+		String msg = "{\"name\": \"홍길동\"}";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, headers, HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		System.out.println("exUpload.............");
+	}
+	
+	
+	@PostMapping("/exUploadpost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		files.forEach(file -> {
+			System.out.println("==================================================");
+			System.out.println("name : " + file.getOriginalFilename());
+			System.out.println("size : " + file.getSize());
+		});
+	}
+	
+	
+	
+	
+	
 	
 }
