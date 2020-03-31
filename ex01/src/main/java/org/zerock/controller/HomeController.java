28c07d6fbcf7f15a1915fdaa87a32085734dee24
ksh030506 <ksh030506@naver.com>
2020-03-31 18:47:49 +0900

    add Param
    Get으로 파라미터를 다루어 보았다

--- ex01/src/main/java/org/zerock/controller/HomeController.java
+++ ex01/src/main/java/org/zerock/controller/HomeController.java
@@ -11,8 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import lombok.extern.log4j.Log4j;
-
 /**
  * Handles requests for the application home page.
  */
@@ -34,6 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
 		return "home";
 	}
 	
