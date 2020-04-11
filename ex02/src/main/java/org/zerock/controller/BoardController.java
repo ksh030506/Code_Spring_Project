afa44192a9b5aaa8dad2b4376097b3618d51d6b6
ksh030506 <ksh030506@naver.com>
2020-04-12 02:41:56 +0900

    add 수정,삭제 페이징 ( 전체 페이징 처리 )
    1. 수정과 삭제페이지로 이동하여 다시 리스트로 갈때 페이지 이동이 없게 만들었다.
    2. Mybatis에서 전체 데이터의 개수 처리
    boardMapper인터페이스에 추가 -> xml에 쿼리 기록 -> boardServiece에 인터페이스 만들기 -> boardServicempl에 오버라이드하여 메소드 기록 -> boardController에 메소드 수정
    
    여기까지 조회, 등록, 수정, 페이징 처리가 완료 되었다.
    
    여기는 중요한 부분이므로 책도 다시 한번 읽어보고 이해하며 공부하여 외워야 한다.!!!
    
    중요!!!
    > 페이징 처리

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -21,72 +21,78 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/board/*")
 @AllArgsConstructor
 public class BoardController {
-	
+
 	private BoardService boardService;
-	
+
 //	@GetMapping("/list")
 //	public void list(Model model) {
 //		log.info("list...........");
 //		model.addAttribute("list", boardService.getList());
 //	}
-	
+
 	@GetMapping("list")
 	public void list(Criteria cri, Model model) {
-		
+
 		log.info("list : " + cri);
 		model.addAttribute("list", boardService.getList(cri));
-		model.addAttribute("pageMaker", new PageDTO(cri, 123));
+		//model.addAttribute("pageMaker", new PageDTO(cri, 123));
+		
+		int total = boardService.getTotal(cri);
+		
+		log.info("total : " + total);
+		
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
 
 	}
-	
-	
+
 	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		log.info("register..... : " + board);
-		
+
 		boardService.register(board);
-		
+
 		rttr.addFlashAttribute("result", board.getBno());
-		
+
 		return "redirect:/board/list";
 	}
+
 	@GetMapping("/register")
 	public void register() {
-		
+
 	}
-	
-	
-	
-	
-	@GetMapping({"/get", "/modify"})
-	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri")
-	Criteria cri, Model model) {
+
+	@GetMapping({ "/get", "/modify" })
+	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
 		log.info("get or modify.......");
-		
+
 		model.addAttribute("board", boardService.get(bno));
 	}
-	
-	
-	
-	
+
 	@PostMapping("/modify")
-	public String modify(BoardVO board, RedirectAttributes rttr) {
+	public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
 		log.info("modify....." + board);
-		
-		if(boardService.modify(board)) {
+
+		if (boardService.modify(board)) {
 			rttr.addFlashAttribute("result", "success");
 		}
-		
+
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+
 		return "redirect:/board/list";
 	}
-	
+
 	@PostMapping("/remove")
-	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+	public String remove(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
 		log.info("remove....." + bno);
-		
-		if(boardService.remove(bno)) {
+
+		if (boardService.remove(bno)) {
 			rttr.addFlashAttribute("result", "success");
 		}
+
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+
 		return "redirect:/board/list";
 	}
 
--- ex02/src/main/webapp/WEB-INF/views/board/modify.jsp
+++ ex02/src/main/webapp/WEB-INF/views/board/modify.jsp
@@ -22,6 +22,9 @@
       <div class="panel-body">
       
       <form role="form" action="/board/modify" method="post">
+      
+      	<input type="hidden" name="pageNum" value="<c:out value="${cri.pageNum }"/>">
+       	<input type="hidden" name="amount" value="<c:out value="${cri.amount }"/>">
 
         <div class="form-group">
         	<label>Bno</label>
@@ -62,32 +65,43 @@
 
 
 <script type="text/javascript">
+$(document).ready(function() {
 
-	$(document).ready(function() {
 
-		var formObj = $("form");
+	  var formObj = $("form");
 
-		$('button').on("click", function(e) {
+	  $('button').on("click", function(e){
+	    
+	    e.preventDefault(); 
+	    
+	    var operation = $(this).data("oper");
+	    
+	    console.log(operation);
+	    
+	    if(operation === 'remove'){
+	      formObj.attr("action", "/board/remove");
+	      
+	    }else if(operation === 'list'){
+	      //move to list
+	      formObj.attr("action", "/board/list").attr("method","get");
+	      
+	      var pageNumTag = $("input[name='pageNum']").clone();
+	      var amountTag = $("input[name='amount']").clone();
+	      var keywordTag = $("input[name='keyword']").clone();
+	      var typeTag = $("input[name='type']").clone();      
+	      
+	      formObj.empty();
+	      
+	      formObj.append(pageNumTag);
+	      formObj.append(amountTag);
+	      formObj.append(keywordTag);
+	      formObj.append(typeTag);	       
+	    }
+	    
+	    formObj.submit();
+	  });
 
-			e.preventDefault();
-
-			var operation = $(this).data("oper");
-
-			console.log(operation);
-
-			if (operation === 'remove') {
-				formObj.attr("action", "/board/remove");
-
-			} else if (operation === 'list') {
-				//move to list
-				self.location = "/board/list";
-				return;
-			}
-
-			formObj.submit();
-		});
-
-	});
+});
 </script>
 
 
