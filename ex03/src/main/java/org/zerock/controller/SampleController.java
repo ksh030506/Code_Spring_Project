0abfea3f4e752dd9919ad67d13b990fd5273733d
ksh030506 <ksh030506@naver.com>
2020-04-13 22:25:17 +0900

    객체의 반환
    객체 반환은 JSON이나 XML을 이용합니다.
    VO 클래스를 생성해 데이터를 입력하고
    MediaType를 이용하여 객체를 웹페이지에 반환을 하였습니다.
    Content-Type도 알아보았습니다.

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zercok.domain.SampleVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -21,5 +22,21 @@ public class SampleController {
 		return "안녕하세요";
 
 	}
+	
+	@GetMapping(value = "/getSample", produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
+	public SampleVO getSample() {
+		return new SampleVO(122, "스타", "로드");
+	}
+	
+	@GetMapping(value = "/getSample2")
+	public SampleVO getSample2() {
+		return new SampleVO(222, "스타", "랙스");
+	}
+	
+	
+	
+	
 
 }
